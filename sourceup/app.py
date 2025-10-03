ceda87a5657f30ca7204a50e066a9b8e46b8359d
iredstonne <iredstonne.dev@gmail.com>
2025-10-03 16:54:56 +0200

    Update app launcher

--- sourceup/app.py
+++ sourceup/app.py
@@ -1,14 +1,15 @@
 import signal
 import sys
-
 from PySide6.QtWidgets import QApplication
-
 from sourceup.window.main import MainWindow
-
 
 def main():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
+    signal.signal(signal.SIGTERM, signal.SIG_DFL)
     app = QApplication()
+    app.setApplicationName("SourceUp")
+    app.setApplicationVersion("0.1.0")
+    app.setOrganizationName("SourceUp")
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec())

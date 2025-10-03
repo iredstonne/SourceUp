51f4bd302f33617fda1c949b813f781ad41998b6
iredstonne <iredstonne.dev@gmail.com>
2025-10-03 16:55:10 +0200

    Remove legacy code

--- sourceup/client/zotero_client.py
+++ sourceup/client/zotero_client.py
@@ -1,59 +1,22 @@
-from pyzotero import zotero
+from typing import TYPE_CHECKING, Iterable
+if TYPE_CHECKING: from sourceup.data.library.ZoteroLibrary import ZoteroLibrary
+if TYPE_CHECKING: from sourceup.data.collection.ZoteroCollection import ZoteroCollection
+if TYPE_CHECKING: from sourceup.data.item.ZoteroItem import ZoteroItem
+from sourceup.client.PyZoteroClient import PyZoteroClient
+from sourceup.client.ZoteroRepository import ZoteroRepository
 
-from sourceup.data.zotero_collection import ZoteroCollection
-from sourceup.data.zotero_item import ZoteroItem
-from sourceup.data.zotero_library import ZoteroLibrary, LibraryType
+def fetch_items(library: "ZoteroLibrary") -> Iterable["ZoteroItem"]:
+    client = PyZoteroClient.from_library(library)
+    repository = ZoteroRepository(client)
+    return repository.list_items()
 
+def fetch_collections(library: "ZoteroLibrary") -> Iterable["ZoteroCollection"]:
+    client = PyZoteroClient.from_library(library)
+    repository = ZoteroRepository(client)
+    return repository.list_collections()
 
-def _zotero(library: ZoteroLibrary):
-    return zotero.Zotero(
-        library.library_id,
-        "user" if library.library_type == LibraryType.USER else "group",
-        library.private_key or None,
-    )
+def fetch_collection_items(library: "ZoteroLibrary", collection: "ZoteroCollection") -> Iterable["ZoteroItem"]:
+    client = PyZoteroClient.from_library(library)
+    repository = ZoteroRepository(client)
+    return repository.list_collection_items(collection)
 
-def fetch_collections(library: ZoteroLibrary):
-    z = _zotero(library)
-    collections = []
-    for collection in z.everything(z.collections()):
-        key = (collection.get("key") or "").strip()
-        data = (collection.get("data") or {})
-        name = (data.get("name") or "").strip()
-        #print(json.dumps(data, indent=4))
-        collections.append(ZoteroCollection(
-            key,
-            name
-        ))
-    return collections
-
-def fetch_items_from_collection(library: ZoteroLibrary, collection: ZoteroCollection):
-    z = _zotero(library)
-    items = []
-    for item in z.everything(z.collection_items(collection.key)):
-        key = (item.get("key") or "").strip()
-        data = (item.get("data") or {})
-        title = (data.get("title") or "").strip()
-        creators = (data.get("creators") or [])
-        creators_names = []
-        for creator in creators:
-            if not isinstance(creator, dict):
-                continue
-            creator_name = (creator.get("name") or "").strip()
-            if creator_name:
-                creators_names.append(creator_name)
-                continue
-            first_name = (creator.get("firstName") or "").strip()
-            last_name = (creator.get("lastName") or "").strip()
-            creator_name = first_name + " " + last_name
-            if creator_name:
-                creators_names.append(creator_name)
-        date = (data.get("date") or "").strip()
-        link = (data.get("url") or "").strip()
-        items.append(ZoteroItem(
-            key,
-            title,
-            creators_names,
-            date,
-            link
-        ))
-    return items

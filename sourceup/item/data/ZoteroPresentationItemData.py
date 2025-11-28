849a92f909ce754a15de4909ad6544a73b78081a
iredstonne <iredstonne.dev@gmail.com>
2025-11-28 12:58:13 +0100

    feat(item): Add zotero presentation item data

---
+++ sourceup/item/data/ZoteroPresentationItemData.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass, fields
+from typing import Optional, override, Dict, Any
+from sourceup.item.ZoteroItemType import ZoteroItemType
+from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
+from sourceup.casts import map_to_str
+
+@dataclass(frozen=True, slots=True)
+class ZoteroPresentationItemData(ZoteroBaseItemData):
+    presentation_type: Optional[str] = None
+    place: Optional[str] = None
+    meeting_name: Optional[str] = None
+
+    @override
+    @classmethod
+    def item_type(cls) -> ZoteroItemType:
+        return ZoteroItemType.PRESENTATION
+
+    @override
+    @classmethod
+    def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroPresentationItemData":
+        _base_item_data = ZoteroBaseItemData.map_from_data(_data)
+        return cls(
+            **{_base_item_data_field.name: getattr(_base_item_data, _base_item_data_field.name)
+               for _base_item_data_field in fields(ZoteroBaseItemData)},
+            presentation_type=map_to_str(_data.get("presentationType")),
+            place=map_to_str(_data.get("place")),
+            meeting_name=map_to_str(_data.get("meetingName"))
+        )

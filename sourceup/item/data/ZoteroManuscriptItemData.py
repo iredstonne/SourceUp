6aa918df0ea92f34c218fe49256c37ddb167cdc8
iredstonne <iredstonne.dev@gmail.com>
2025-11-28 12:56:56 +0100

    feat(item): Add zotero manuscript item data

---
+++ sourceup/item/data/ZoteroManuscriptItemData.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass, fields
+from typing import Optional, override, Dict, Any
+from sourceup.item.ZoteroItemType import ZoteroItemType
+from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
+from sourceup.casts import map_to_str
+
+@dataclass(frozen=True, slots=True)
+class ZoteroManuscriptItemData(ZoteroBaseItemData):
+    manuscript_type: Optional[str] = None
+    place: Optional[str] = None
+    num_pages: Optional[str] = None
+
+    @override
+    @classmethod
+    def item_type(cls) -> ZoteroItemType:
+        return ZoteroItemType.MANUSCRIPT
+
+    @override
+    @classmethod
+    def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroManuscriptItemData":
+        _base_item_data = ZoteroBaseItemData.map_from_data(_data)
+        return cls(
+            **{_base_item_data_field.name: getattr(_base_item_data, _base_item_data_field.name)
+               for _base_item_data_field in fields(ZoteroBaseItemData)},
+            manuscript_type=map_to_str(_data.get("manuscriptType")),
+            place=map_to_str(_data.get("place")),
+            num_pages=map_to_str(_data.get("numPages"))
+        )

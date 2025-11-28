937475b71ef1206bd5c705fa84855068d91233c0
iredstonne <iredstonne.dev@gmail.com>
2025-11-28 22:04:10 +0100

    feat(item data): Add patent item data

---
+++ sourceup/item/data/ZoteroPatentItemData.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass, fields
+from typing import Optional, override, Dict, Any
+from sourceup.item.ZoteroItemType import ZoteroItemType
+from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
+from sourceup.casts import map_to_str
+
+@dataclass(frozen=True, slots=True)
+class ZoteroPatentItemData(ZoteroBaseItemData):
+    place: Optional[str] = None
+    country: Optional[str] = None
+    assignee: Optional[str] = None
+    issuing_authority: Optional[str] = None
+    patent_number: Optional[str] = None
+    filling_date: Optional[str] = None
+    pages: Optional[str] = None
+    application_number: Optional[str] = None
+    priority_numbers: Optional[str] = None
+    issue_date: Optional[str] = None
+    references: Optional[str] = None
+    legal_status: Optional[str] = None
+
+    @override
+    @classmethod
+    def item_type(cls) -> ZoteroItemType:
+        return ZoteroItemType.PATENT
+
+    @override
+    @classmethod
+    def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroPatentItemData":
+        _base_item_data = ZoteroBaseItemData.map_from_data(_data)
+        return cls(
+            **{base_item_data_field.name: getattr(_base_item_data, base_item_data_field.name)
+               for base_item_data_field in fields(ZoteroBaseItemData)},
+            place=map_to_str(_data.get("place")),
+            country=map_to_str(_data.get("country")),
+            assignee=map_to_str(_data.get("assignee")),
+            issuing_authority=map_to_str(_data.get("issuingAuthority")),
+            patent_number=map_to_str(_data.get("patentNumber")),
+            filling_date=map_to_str(_data.get("fillingDate")),
+            pages=map_to_str(_data.get("pages")),
+            application_number=map_to_str(_data.get("applicationNumber")),
+            priority_numbers=map_to_str(_data.get("priorityNumbers")),
+            issue_date=map_to_str(_data.get("issueDate")),
+            references=map_to_str(_data.get("references")),
+            legal_status=map_to_str(_data.get("legalStatus"))
+        )

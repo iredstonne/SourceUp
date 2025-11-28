8953b88ee3cf87a940ac3c1eebe99778dde961f2
iredstonne <iredstonne.dev@gmail.com>
2025-11-28 13:04:15 +0100

    feat(item): Add zotero forum post item data

---
+++ sourceup/item/data/ZoteroForumPostItemData.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass, fields
+from typing import Optional, override, Dict, Any
+from sourceup.item.ZoteroItemType import ZoteroItemType
+from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
+from sourceup.casts import map_to_str
+
+@dataclass(frozen=True, slots=True)
+class ZoteroForumPostItemData(ZoteroBaseItemData):
+    blog_title: Optional[str] = None
+    website_type: Optional[str] = None
+
+    @override
+    @classmethod
+    def item_type(cls) -> ZoteroItemType:
+        return ZoteroItemType.FORUM_POST
+
+    @override
+    @classmethod
+    def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroForumPostItemData":
+        _base_item_data = ZoteroBaseItemData.map_from_data(_data)
+        return cls(
+            **{_base_item_data_field.name: getattr(_base_item_data, _base_item_data_field.name)
+               for _base_item_data_field in fields(ZoteroBaseItemData)},
+            blog_title=map_to_str(_data.get("blogTitle")),
+            website_type=map_to_str(_data.get("websiteType"))
+        )

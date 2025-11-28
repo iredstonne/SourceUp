0af75815877b405415da6583ebc62cceacc54b49
iredstonne <iredstonne.dev@gmail.com>
2025-11-28 13:05:33 +0100

    feat(item): Add zotero podcast item data

---
+++ sourceup/item/data/ZoteroPodcastItemData.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass, fields
+from typing import Optional, override, Dict, Any
+from sourceup.item.ZoteroItemType import ZoteroItemType
+from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
+from sourceup.casts import map_to_str
+
+@dataclass(frozen=True, slots=True)
+class ZoteroPodcastItemData(ZoteroBaseItemData):
+    series_title: Optional[str] = None
+    episode_number: Optional[str] = None
+    audio_file_type: Optional[str] = None
+    running_time: Optional[str] = None
+
+    @override
+    @classmethod
+    def item_type(cls) -> ZoteroItemType:
+        return ZoteroItemType.PODCAST
+
+    @override
+    @classmethod
+    def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroPodcastItemData":
+        _base_item_data = ZoteroBaseItemData.map_from_data(_data)
+        return cls(
+            **{_base_item_data_field.name: getattr(_base_item_data, _base_item_data_field.name)
+               for _base_item_data_field in fields(ZoteroBaseItemData)},
+            series_title=map_to_str(_data.get("seriesTitle")),
+            episode_number=map_to_str(_data.get("episodeNumber")),
+            audio_file_type=map_to_str(_data.get("audioFileType")),
+            running_time=map_to_str(_data.get("runningTime"))
+        )

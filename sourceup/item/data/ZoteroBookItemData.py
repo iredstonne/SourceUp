4ea3cff7b002c8fa00b60fa178888e165f24f8cb
iredstonne <iredstonne.dev@gmail.com>
2025-11-28 12:55:35 +0100

    feat(item): Add zotero book item data

---
+++ sourceup/item/data/ZoteroBookItemData.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass, fields
+from typing import Optional, override, Dict, Any
+from sourceup.item.ZoteroItemType import ZoteroItemType
+from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
+from sourceup.casts import map_to_str
+
+@dataclass(frozen=True, slots=True)
+class ZoteroBookItemData(ZoteroBaseItemData):
+    series: Optional[str] = None
+    series_number: Optional[str] = None
+    volume: Optional[str] = None
+    number_of_volumes: Optional[str] = None
+    edition: Optional[str] = None
+    place: Optional[str] = None
+    publisher: Optional[str] = None
+    num_pages: Optional[str] = None
+    isbn: Optional[str] = None
+
+    @override
+    @classmethod
+    def item_type(cls) -> ZoteroItemType:
+        return ZoteroItemType.BOOK
+
+    @override
+    @classmethod
+    def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroBookItemData":
+        _base_item_data = ZoteroBaseItemData.map_from_data(_data)
+        return cls(
+            **{_base_item_data_field.name: getattr(_base_item_data, _base_item_data_field.name)
+               for _base_item_data_field in fields(ZoteroBaseItemData)},
+            series=map_to_str(_data.get("series")),
+            series_number=map_to_str(_data.get("seriesNumber")),
+            volume=map_to_str(_data.get("volume")),
+            number_of_volumes=map_to_str(_data.get("numberOfVolumes")),
+            edition=map_to_str(_data.get("edition")),
+            place=map_to_str(_data.get("place")),
+            publisher=map_to_str(_data.get("publisher")),
+            num_pages=map_to_str(_data.get("numPages")),
+            isbn=map_to_str(_data.get("isbn"))
+        )

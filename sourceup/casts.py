21e1c8e37f30610ae0bbe209b8d8c3330b6d7f66
iredstonne <iredstonne.dev@gmail.com>
2025-11-28 12:18:17 +0100

    feat(casts): Improve casts methods

--- sourceup/casts.py
+++ sourceup/casts.py
@@ -1,16 +1,18 @@
 from typing import Any, Dict, Tuple, Iterable
 
+def map_to_str(_value: Any) -> str:
+    return _value if isinstance(_value, str) else ""
 
-def map_to_str(value: Any) -> str:
-    return value if isinstance(value, str) else ""
+def map_to_dict(_value: Any) -> Dict[str, Any]:
+    return _value if isinstance(_value, dict) else {}
 
-def map_to_dict(value: Any) -> Dict[str, Any]:
-    return value if isinstance(value, dict) else {}
-
-def map_to_tuple_of_dicts(sequence: Iterable[Any]) -> Tuple[Dict[str, Any], ...]:
-    if isinstance(sequence, tuple):
-        return tuple(value for value in sequence if isinstance(value, dict))
+def map_to_tuple_of_dicts(_sequence: Iterable[Any]) -> Tuple[Dict[str, Any], ...]:
+    if isinstance(_sequence, tuple):
+        return tuple(_sequence_value for _sequence_value in _sequence if isinstance(_sequence_value, dict))
     return ()
 
-def normalize_str(value: str) -> str:
-    return value.strip()
+def normalize_str_case_sensitive(_value: str) -> str:
+    return map_to_str(_value).strip()
+
+def normalize_str_case_insensitive(_value: str) -> str:
+    return normalize_str_case_sensitive(_value).lower()

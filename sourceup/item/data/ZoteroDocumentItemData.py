f4621dc4aa62f66f4b3314bfa3b7953447b69c59
iredstonne <iredstonne.dev@gmail.com>
2025-11-28 12:57:25 +0100

    feat(item): Add zotero document item data

---
+++ sourceup/item/data/ZoteroDocumentItemData.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, fields
+from typing import Optional, override, Dict, Any
+from sourceup.item.ZoteroItemType import ZoteroItemType
+from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
+from sourceup.casts import map_to_str
+
+@dataclass(frozen=True, slots=True)
+class ZoteroDocumentItemData(ZoteroBaseItemData):
+    publisher: Optional[str] = None
+
+    @override
+    @classmethod
+    def item_type(cls) -> ZoteroItemType:
+        return ZoteroItemType.DOCUMENT
+
+    @override
+    @classmethod
+    def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroDocumentItemData":
+        _base_item_data = ZoteroBaseItemData.map_from_data(_data)
+        return cls(
+            **{_base_item_data_field.name: getattr(_base_item_data, _base_item_data_field.name)
+               for _base_item_data_field in fields(ZoteroBaseItemData)},
+            publisher=map_to_str(_data.get("publisher"))
+        )

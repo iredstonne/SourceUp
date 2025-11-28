3fcc12a02521f23d538495c47032dc765e33efc0
iredstonne <iredstonne.dev@gmail.com>
2025-11-28 12:32:37 +0100

    feat(collection): Update collection data class

--- sourceup/collection/ZoteroCollection.py
+++ sourceup/collection/ZoteroCollection.py
@@ -2,11 +2,11 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True, slots=True)
 class ZoteroCollection:
-    key: str
-    name: str
+    collection_key: str
+    collection_name: str
 
     def __repr__(self):
-        return f"ZoteroCollection({self.key}, {self.name})"
+        return self.collection_name
 
     def __str__(self):
         return self.__repr__()

245ac705361ea1068220696748b1f226d660bda4
iredstonne <iredstonne.dev@gmail.com>
2025-11-28 13:15:32 +0100

    feat(model data provider): Add collection implementation of default data provider

---
+++ sourceup/ui/model/data_provider/ZoteroCollectionDataProvider.py
@@ -0,0 +1,26 @@
+from PySide6.QtCore import Qt
+from typing import override, Set, Final, Union, Optional
+from sourceup.collection.ZoteroCollection import ZoteroCollection
+from sourceup.ui.model.data_provider.DataProviderProtocol import DataProviderProtocol
+
+class ZoteroCollectionDataProvider(DataProviderProtocol[ZoteroCollection]):
+    COLLECTION_USER_ROLE: Final[int] = DataProviderProtocol.USER_ROLE + 1
+
+    @override
+    def roles(self) -> Set[int]:
+        return {
+            *super().roles(),
+            ZoteroCollectionDataProvider.COLLECTION_USER_ROLE
+        }
+
+    @override
+    def data(self, _collection: ZoteroCollection, _role: Qt.ItemDataRole) -> Optional[ZoteroCollection | str]:
+        match _role:
+            case ZoteroCollectionDataProvider.COLLECTION_USER_ROLE:
+                return _collection
+            case DataProviderProtocol.DISPLAY_ROLE | DataProviderProtocol.TOOLTIP_ROLE:
+                return str(_collection)
+
+    @override
+    def flags(self, _collection: ZoteroCollection) -> Union[Qt.ItemFlag]:
+        return Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsSelectable # type: ignore

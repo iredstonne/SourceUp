25192904d5c0faebf1ae912fd95225a816366f61
iredstonne <iredstonne.dev@gmail.com>
2025-11-28 13:23:09 +0100

    feat(background): Add zotero library fetch dialog for background presentation

---
+++ sourceup/ui/window/ZoteroLibraryFetchDialog.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QDialog, QWidget, QVBoxLayout, QLabel
+
+class ZoteroLibraryFetchDialog(QDialog):
+    def __init__(self, parent: Optional[QWidget] = None):
+        super().__init__(parent)
+        self.setWindowTitle("Zotero")
+        self.setModal(True)
+        self._layout = QVBoxLayout()
+        self._label = QLabel("Fetching data from Zotero…", self)
+        self._label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self._layout.addWidget(self._label)
+        self.setLayout(self._layout)

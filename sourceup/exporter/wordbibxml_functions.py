943aab541f3d531d9519bbdcc5d0464d0f10ee78
iredstonne <iredstonne.dev@gmail.com>
2025-12-04 09:25:31 +0100

    fix(exporter > wordbibxml): Make wordbibxml_functions file more straightforward to read and improve IO error message

--- sourceup/exporter/wordbibxml_functions.py
+++ sourceup/exporter/wordbibxml_functions.py
@@ -1,6 +1,6 @@
 from typing import Iterable
 from sourceup.item.ZoteroItem import ZoteroItem
-from xml.etree.ElementTree import register_namespace, Element, SubElement, ElementTree
+from xml.etree.ElementTree import register_namespace, Element, ElementTree
 
 BIBLIOGRAPHY_NS = "http://schemas.openxmlformats.org/officeDocument/2006/bibliography"
 register_namespace("b", BIBLIOGRAPHY_NS)
@@ -8,25 +8,17 @@ register_namespace("b", BIBLIOGRAPHY_NS)
 def create_bibliography_namespaced_element(_tag_name: str):
     return Element(f"{{{BIBLIOGRAPHY_NS}}}{_tag_name}")
 
-def create_bibliography_namespaced_sub_element(_parent_element: Element, _tag_name: str):
-    return SubElement(_parent_element, f"{{{BIBLIOGRAPHY_NS}}}{_tag_name}")
-
-def create_bibxml_sources_element() -> Element:
-    return create_bibliography_namespaced_element("Sources")
-
-def create_bibxml_source_element(_item: ZoteroItem) -> Element:
-    _source_element = create_bibliography_namespaced_element("Source")
-    _tag_element = create_bibliography_namespaced_sub_element(_source_element, "Tag")
-    _tag_element.text = _item.item_key
-    _item.item_data.map_to_bibxml(_source_element)
-    return _source_element
-
-def export_as_bibxml_to_output_file(_items: Iterable[ZoteroItem], _output_file_save_path: str):
+def export_as_bibxml_to_output_file(_items: Iterable[ZoteroItem], _output_file_save_path: str) -> str:
     # <b:Sources>
-    _sources_element = create_bibxml_sources_element()
+    _sources_element = create_bibliography_namespaced_element("Sources")
     for _item in _items:
         # <b:Source>
-        _source_element = create_bibxml_source_element(_item)
+        _source_element = create_bibliography_namespaced_element("Source")
+        # <b:Tag>
+        _tag_element = create_bibliography_namespaced_element("Tag")
+        _tag_element.text = _item.item_key
+        _source_element.append(_tag_element)
+        _item.item_data.map_to_bibxml(_source_element)
         _sources_element.append(_source_element)
     _tree = ElementTree(_sources_element)
     _tree.write(_output_file_save_path, xml_declaration=True, encoding="UTF-8")
@@ -34,5 +26,5 @@ def export_as_bibxml_to_output_file(_items: Iterable[ZoteroItem], _output_file_s
 
 def decipher_bibxml_export_error(e: Exception) -> str:
     if isinstance(e, (OSError, IOError)):
-        return "Unable to write BibXML to disk"
+        return "Unable to write BibXML export file to file system"
     return str(e)

935b0f5919d2aed74baafe4d3eb92b350398ca85
iredstonne <iredstonne.dev@gmail.com>
2025-12-04 10:53:23 +0100

    feat(exporter > wordbibxml): Add BibXML tags if missing and create a function to add common book tags for books and book sections

--- sourceup/exporter/wordbibxml_functions.py
+++ sourceup/exporter/wordbibxml_functions.py
@@ -1,12 +1,44 @@
-from typing import Iterable
+from typing import Iterable, Optional, Any
 from sourceup.item.ZoteroItem import ZoteroItem
 from xml.etree.ElementTree import register_namespace, Element, ElementTree
 
 BIBLIOGRAPHY_NS = "http://schemas.openxmlformats.org/officeDocument/2006/bibliography"
 register_namespace("b", BIBLIOGRAPHY_NS)
 
-def create_bibliography_namespaced_element(_tag_name: str):
+def create_bibliography_namespaced_element(_tag_name: str) -> Element:
     return Element(f"{{{BIBLIOGRAPHY_NS}}}{_tag_name}")
+
+def find_bibliography_namespaced_element(_source_element: Element, _tag_name: str) -> Optional[Element]:
+    return _source_element.find(f"{{{BIBLIOGRAPHY_NS}}}{_tag_name}")
+
+def add_bibliography_namespaced_element_if_missing(_source_element: Element, _tag_name: str, _tag_value: Any):
+    if _tag_value:
+        _element = find_bibliography_namespaced_element(
+            _source_element, _tag_name)
+        if _element:
+            _element.text = str(_tag_value)
+        else:
+            _element = create_bibliography_namespaced_element(_tag_name)
+            _element.text = str(_tag_value)
+            _source_element.append(_element)
+
+def add_common_book_bibliography_namespaced_element(
+    _source_element: Element,
+    _volume,
+    _number_volumes,
+    _edition,
+    _city,
+    _publisher,
+    _pages,
+    _standard_number
+):
+    add_bibliography_namespaced_element_if_missing(_source_element, "Volume", _volume)
+    add_bibliography_namespaced_element_if_missing(_source_element, "NumberVolumes", _number_volumes)
+    add_bibliography_namespaced_element_if_missing(_source_element, "Edition", _edition)
+    add_bibliography_namespaced_element_if_missing(_source_element, "City", _city)
+    add_bibliography_namespaced_element_if_missing(_source_element, "Publisher", _publisher)
+    add_bibliography_namespaced_element_if_missing(_source_element, "Pages", _pages)
+    add_bibliography_namespaced_element_if_missing(_source_element, "StandardNumber", _standard_number)
 
 def export_as_bibxml_to_output_file(_items: Iterable[ZoteroItem], _output_file_save_path: str) -> str:
     # <b:Sources>

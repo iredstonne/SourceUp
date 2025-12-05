c7c269442b337ee9d6195377cc34c1cddfa1136b
iredstonne <iredstonne.dev@gmail.com>
2025-12-05 14:13:48 +0100

    fix/feat(item data > book section): Add BibXML support for conference paper item data

--- sourceup/item/data/ZoteroConferencePaperItemData.py
+++ sourceup/item/data/ZoteroConferencePaperItemData.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.casts import map_to_str
@@ -23,6 +25,11 @@ class ZoteroConferencePaperItemData(ZoteroBaseItemData):
 
     @override
     @classmethod
+    def bibliography_source_type(cls):
+        return "ConferenceProceedings"
+
+    @override
+    @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroConferencePaperItemData":
         _base_item_data = ZoteroBaseItemData.map_from_data(_data)
         return cls(
@@ -35,6 +42,19 @@ class ZoteroConferencePaperItemData(ZoteroBaseItemData):
             volume=map_to_str(_data.get("volume")),
             pages=map_to_str(_data.get("pages")),
             series=map_to_str(_data.get("series")),
-            doi=map_to_str(_data.get("doi")),
-            isbn=map_to_str(_data.get("isbn"))
+            doi=map_to_str(_data.get("DOI")),
+            isbn=map_to_str(_data.get("ISBN"))
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "BookTitle", self.proceedings_title)
+        add_bibliography_namespaced_element_if_missing(_source_element, "ConferenceName", self.conference_name)
+        add_bibliography_namespaced_element_if_missing(_source_element, "City", self.place)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Publisher", self.publisher)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Volume", self.volume)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Pages", self.pages)
+        add_bibliography_namespaced_element_if_missing(_source_element, "DOI", self.doi)
+        add_bibliography_namespaced_element_if_missing(_source_element, "StandardNumber", self.isbn)

3537a6836598307420a35694b308d0c721254b38
iredstonne <iredstonne.dev@gmail.com>
2025-12-05 14:16:37 +0100

    fix/feat(item data > hearing item data): Add BibXML support for hearing item data

--- sourceup/item/data/ZoteroHearingItemData.py
+++ sourceup/item/data/ZoteroHearingItemData.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.casts import map_to_str
@@ -23,6 +26,11 @@ class ZoteroHearingItemData(ZoteroBaseItemData):
 
     @override
     @classmethod
+    def bibliography_source_type(cls):
+        return "Hearing"
+
+    @override
+    @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroHearingItemData":
         _base_item_data = ZoteroBaseItemData.map_from_data(_data)
         return cls(
@@ -31,10 +39,24 @@ class ZoteroHearingItemData(ZoteroBaseItemData):
             committee=map_to_str(_data.get("committee")),
             place=map_to_str(_data.get("place")),
             publisher=map_to_str(_data.get("publisher")),
-            number_of_volumes=map_to_str(_data.get("number_of_volumes")),
-            document_number=map_to_str(_data.get("document_number")),
+            number_of_volumes=map_to_str(_data.get("numberOfVolumes")),
+            document_number=map_to_str(_data.get("documentNumber")),
             pages=map_to_str(_data.get("pages")),
-            legislative_body=map_to_str(_data.get("legislative_body")),
+            legislative_body=map_to_str(_data.get("legislativeBody")),
             session=map_to_str(_data.get("session")),
             history=map_to_str(_data.get("history"))
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "Department", self.committee)
+        add_bibliography_namespaced_element_if_missing(_source_element, "City", self.place)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Publisher", self.publisher)
+        add_bibliography_namespaced_element_if_missing(_source_element, "NumberVolumes", self.number_of_volumes)
+        add_bibliography_namespaced_element_if_missing(_source_element, "StandardNumber", self.document_number)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Pages", self.pages)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Institution", self.legislative_body)
+        add_bibliography_namespaced_element_if_missing(_source_element, "ConferenceName", self.session)
+

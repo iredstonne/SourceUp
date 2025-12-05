e97d807c45420dddbdc3cd855b4450611788739a
iredstonne <iredstonne.dev@gmail.com>
2025-12-05 14:17:23 +0100

    fix/feat(item data > journal article item data): Add BibXML support for journal article item data

--- sourceup/item/data/ZoteroJournalArticleItemData.py
+++ sourceup/item/data/ZoteroJournalArticleItemData.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.casts import map_to_str
@@ -13,7 +16,7 @@ class ZoteroJournalArticleItemData(ZoteroBaseItemData):
     series: Optional[str] = None
     series_title: Optional[str] = None
     series_text: Optional[str] = None
-    journal_abreviation: Optional[str] = None
+    journal_abbreviation: Optional[str] = None
     doi: Optional[str] = None
     issn: Optional[str] = None
 
@@ -21,6 +24,11 @@ class ZoteroJournalArticleItemData(ZoteroBaseItemData):
     @classmethod
     def item_type(cls) -> ZoteroItemType:
         return ZoteroItemType.JOURNAL_ARTICLE
+
+    @override
+    @classmethod
+    def bibliography_source_type(cls):
+        return "JournalArticle"
 
     @override
     @classmethod
@@ -34,9 +42,22 @@ class ZoteroJournalArticleItemData(ZoteroBaseItemData):
             issue=map_to_str(_data.get("issue")),
             pages=map_to_str(_data.get("pages")),
             series=map_to_str(_data.get("series")),
-            series_title=map_to_str(_data.get("series_title")),
-            series_text=map_to_str(_data.get("series_text")),
-            journal_abreviation=map_to_str(_data.get("journal_abreviation")),
-            doi=map_to_str(_data.get("doi")),
-            issn=map_to_str(_data.get("issn"))
+            series_title=map_to_str(_data.get("seriesTitle")),
+            series_text=map_to_str(_data.get("seriesText")),
+            journal_abbreviation=map_to_str(_data.get("journalAbbreviation")),
+            doi=map_to_str(_data.get("DOI")),
+            issn=map_to_str(_data.get("ISSN"))
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "JournalName", self.publication_title)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Volume", self.volume)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Issue", self.issue)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Pages", self.pages)
+        add_bibliography_namespaced_element_if_missing(_source_element, "DOI", self.doi)
+        add_bibliography_namespaced_element_if_missing(_source_element, "StandardNumber", self.issn)
+
+

a6d3b130ff8b33deddc84588bb60bd4cafc7155d
iredstonne <iredstonne.dev@gmail.com>
2025-12-05 14:12:56 +0100

    fix/feat(item data > audio recording): Add BibXML support for audio recording item data

--- sourceup/item/data/ZoteroAudioRecordingItemData.py
+++ sourceup/item/data/ZoteroAudioRecordingItemData.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.data.ZoteroRecordingItemData import ZoteroRecordingItemData
 from sourceup.casts import map_to_str
@@ -15,6 +18,11 @@ class ZoteroAudioRecordingItemData(ZoteroRecordingItemData):
 
     @override
     @classmethod
+    def bibliography_source_type(cls):
+        return "SoundRecording"
+
+    @override
+    @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroAudioRecordingItemData":
         _recording_item_data = ZoteroRecordingItemData.map_from_data(_data)
         return cls(
@@ -22,3 +30,9 @@ class ZoteroAudioRecordingItemData(ZoteroRecordingItemData):
                for _recording_item_data_field in fields(ZoteroRecordingItemData)},
             audio_recording_format=map_to_str(_data.get("audioRecordingFormat"))
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroRecordingItemData.map_to_bibxml(self, _source_element)
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "Medium", self.audio_recording_format)

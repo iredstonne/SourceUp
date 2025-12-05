c6b116cf3f0bd333f71dbd6aaec37555fe347603
iredstonne <iredstonne.dev@gmail.com>
2025-12-05 14:20:53 +0100

    fix/feat(item data > tv broadcast item data): Add BibXML support for tv broadcast item data

--- sourceup/item/data/ZoteroTvBroadcastItemData.py
+++ sourceup/item/data/ZoteroTvBroadcastItemData.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.data.ZoteroBroadcastItemData import ZoteroBroadcastItemData
 from sourceup.casts import map_to_str
@@ -15,6 +18,11 @@ class ZoteroTvBroadcastItemData(ZoteroBroadcastItemData):
 
     @override
     @classmethod
+    def bibliography_source_type(cls):
+        return "Film"
+
+    @override
+    @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroTvBroadcastItemData":
         _broadcast_item_data = ZoteroBroadcastItemData.map_from_data(_data)
         return cls(
@@ -22,3 +30,9 @@ class ZoteroTvBroadcastItemData(ZoteroBroadcastItemData):
                for _broadcast_item_data_field in fields(ZoteroBroadcastItemData)},
             video_recording_format=map_to_str(_data.get("videoRecordingFormat"))
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBroadcastItemData.map_to_bibxml(self, _source_element)
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "Medium", self.video_recording_format)

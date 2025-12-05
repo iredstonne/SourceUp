b0f35a15c5574ae0f9fe496dea26b133549b1a67
iredstonne <iredstonne.dev@gmail.com>
2025-12-05 14:14:34 +0100

    fix/feat(item data > dataset): Add BibXML support for dataset item data

--- sourceup/item/data/ZoteroDatasetItemData.py
+++ sourceup/item/data/ZoteroDatasetItemData.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.casts import map_to_str
@@ -22,11 +25,34 @@ class ZoteroDatasetItemData(ZoteroBaseItemData):
 
     @override
     @classmethod
+    def bibliography_source_type(cls):
+        return "ElectronicSource"
+
+    @override
+    @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroDatasetItemData":
         _base_item_data = ZoteroBaseItemData.map_from_data(_data)
         return cls(
             **{_base_item_data_field.name: getattr(_base_item_data, _base_item_data_field.name)
                for _base_item_data_field in fields(ZoteroBaseItemData)},
             identifier=map_to_str(_data.get("identifier")),
-            type=map_to_str(_data.get("type"))
+            type=map_to_str(_data.get("type")),
+            version_number=map_to_str(_data.get("version")),
+            repository=map_to_str(_data.get("repository")),
+            repository_location=map_to_str(_data.get("repositoryLocation")),
+            format=map_to_str(_data.get("format")),
+            doi=map_to_str(_data.get("DOI")),
+            citation_key=map_to_str(_data.get("citationKey")),
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "StandardNumber", self.identifier)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Type", self.type)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Version", self.version_number)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Institution", self.repository)
+        add_bibliography_namespaced_element_if_missing(_source_element, "City", self.repository_location)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Medium", self.format)
+        add_bibliography_namespaced_element_if_missing(_source_element, "DOI", self.doi)

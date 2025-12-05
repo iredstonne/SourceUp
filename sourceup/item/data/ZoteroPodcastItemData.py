d8fb88d53965d014a68a56970435ae2449eb6095
iredstonne <iredstonne.dev@gmail.com>
2025-12-05 14:18:45 +0100

    fix/feat(item data > podcast item data): Add partial BibXML support for podcast item data

--- sourceup/item/data/ZoteroPodcastItemData.py
+++ sourceup/item/data/ZoteroPodcastItemData.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.casts import map_to_str
@@ -18,6 +21,11 @@ class ZoteroPodcastItemData(ZoteroBaseItemData):
 
     @override
     @classmethod
+    def bibliography_source_type(cls):
+        return "SoundRecording"
+
+    @override
+    @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroPodcastItemData":
         _base_item_data = ZoteroBaseItemData.map_from_data(_data)
         return cls(
@@ -28,3 +36,12 @@ class ZoteroPodcastItemData(ZoteroBaseItemData):
             audio_file_type=map_to_str(_data.get("audioFileType")),
             running_time=map_to_str(_data.get("runningTime"))
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "AlbumTitle", self.series_title)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Number", self.episode_number)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Medium", self.audio_file_type)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Duration", self.running_time)

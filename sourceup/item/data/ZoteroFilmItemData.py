5a518dadf1795b7893d879b49e918740167a7bd7
iredstonne <iredstonne.dev@gmail.com>
2025-12-05 14:15:51 +0100

    fix/feat(item data > film): Add BibXML support for film item data

--- sourceup/item/data/ZoteroFilmItemData.py
+++ sourceup/item/data/ZoteroFilmItemData.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.casts import map_to_str
@@ -18,6 +21,11 @@ class ZoteroFilmItemData(ZoteroBaseItemData):
 
     @override
     @classmethod
+    def bibliography_source_type(cls):
+        return "Film"
+
+    @override
+    @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroFilmItemData":
         _base_item_data = ZoteroBaseItemData.map_from_data(_data)
         return cls(
@@ -28,3 +36,12 @@ class ZoteroFilmItemData(ZoteroBaseItemData):
             video_recording_format=map_to_str(_data.get("videoRecordingFormat")),
             running_time=map_to_str(_data.get("runningTime"))
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "Publisher", self.distributor)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Type", self.genre)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Medium", self.video_recording_format)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Duration", self.running_time)

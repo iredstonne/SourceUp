b8808a656edad491cde41cf03a3e03ca45ac1bf9
iredstonne <iredstonne.dev@gmail.com>
2025-12-05 14:19:47 +0100

    fix/feat(item data > presentation item data): Add BibXML support for presentation item data

--- sourceup/item/data/ZoteroPresentationItemData.py
+++ sourceup/item/data/ZoteroPresentationItemData.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.casts import map_to_str
@@ -17,6 +20,11 @@ class ZoteroPresentationItemData(ZoteroBaseItemData):
 
     @override
     @classmethod
+    def bibliography_source_type(cls):
+        return "Misc"
+
+    @override
+    @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroPresentationItemData":
         _base_item_data = ZoteroBaseItemData.map_from_data(_data)
         return cls(
@@ -26,3 +34,11 @@ class ZoteroPresentationItemData(ZoteroBaseItemData):
             place=map_to_str(_data.get("place")),
             meeting_name=map_to_str(_data.get("meetingName"))
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "Type", self.presentation_type)
+        add_bibliography_namespaced_element_if_missing(_source_element, "City", self.place)
+        add_bibliography_namespaced_element_if_missing(_source_element, "ConferenceName", self.meeting_name)

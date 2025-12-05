3a6b4124257d6e4a884461240fb415214d4cc71b
iredstonne <iredstonne.dev@gmail.com>
2025-12-05 14:16:03 +0100

    fix/feat(item data > forum post): Add BibXML support for forum post item data

--- sourceup/item/data/ZoteroForumPostItemData.py
+++ sourceup/item/data/ZoteroForumPostItemData.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.casts import map_to_str
 
 @dataclass(frozen=True, slots=True)
 class ZoteroForumPostItemData(ZoteroBaseItemData):
-    blog_title: Optional[str] = None
+    forum_title: Optional[str] = None
     website_type: Optional[str] = None
 
     @override
@@ -16,11 +19,24 @@ class ZoteroForumPostItemData(ZoteroBaseItemData):
 
     @override
     @classmethod
+    def bibliography_source_type(cls):
+        return "DocumentFromInternetSite"
+
+    @override
+    @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroForumPostItemData":
         _base_item_data = ZoteroBaseItemData.map_from_data(_data)
         return cls(
             **{_base_item_data_field.name: getattr(_base_item_data, _base_item_data_field.name)
                for _base_item_data_field in fields(ZoteroBaseItemData)},
-            blog_title=map_to_str(_data.get("blogTitle")),
+            forum_title=map_to_str(_data.get("forumTitle")),
             website_type=map_to_str(_data.get("websiteType"))
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "InternetSiteTitle", self.forum_title)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Type", self.website_type)
+

fff8dfc5ea81b461313f7b06f5ef23e5c7f08cea
iredstonne <iredstonne.dev@gmail.com>
2025-12-11 22:46:18 +0100

    feat(item data > attachment): Full support of BibXML tags

--- sourceup/item/data/ZoteroAttachmentItemData.py
+++ sourceup/item/data/ZoteroAttachmentItemData.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass, fields
-from typing import Optional, override, Dict, Any
-from sourceup.item.ZoteroItemType import ZoteroItemType
+from typing import override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
-from sourceup.casts import map_to_str
+from sourceup.item.ZoteroItemType import ZoteroItemType
 
 @dataclass(frozen=True, slots=True)
 class ZoteroAttachmentItemData(ZoteroBaseItemData):
@@ -25,3 +27,25 @@ class ZoteroAttachmentItemData(ZoteroBaseItemData):
             **{_base_item_data_field.name: getattr(_base_item_data, _base_item_data_field.name)
                for _base_item_data_field in fields(ZoteroBaseItemData)}
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        # SourceType -> Misc
+        # PublicationTitle: Mapped (title)
+        # MediaType: Mapped "Attachment"
+        # City: Not mapped
+        # StateProvince: Not mapped
+        # CountryRegion: Not mapped
+        # Publisher: Not mapped
+        # Pages: Not mapped
+        # Volume: Not mapped
+        # Edition: Not mapped
+        # Number: Not mapped
+        # StandardNumber: Not mapped
+        # Medium: Not mapped
+        # DOI: Not mapped
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "PublicationTitle", self.title)
+        add_bibliography_namespaced_element_if_missing(_source_element, "MediaType", "Attachment")

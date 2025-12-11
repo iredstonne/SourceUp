8672103f1ec67d0f296e22dd732353b4c3371fcc
iredstonne <iredstonne.dev@gmail.com>
2025-12-11 22:51:13 +0100

    feat(item data > standard): Full support of BibXML tags

--- sourceup/item/data/ZoteroStandardItemData.py
+++ sourceup/item/data/ZoteroStandardItemData.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.casts import map_to_str
@@ -20,6 +23,11 @@ class ZoteroStandardItemData(ZoteroBaseItemData):
 
     @override
     @classmethod
+    def bibliography_source_type(cls):
+        return "Report"
+
+    @override
+    @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroStandardItemData":
         _base_item_data = ZoteroBaseItemData.map_from_data(_data)
         return cls(
@@ -32,3 +40,25 @@ class ZoteroStandardItemData(ZoteroBaseItemData):
             version_number=map_to_str(_data.get("versionNumber")),
             status=map_to_str(_data.get("status"))
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        # SourceType -> Report
+        # Department: Mapped (committee)
+        # Institution: Mapped (organization)
+        # Publisher: Mapped (organization)
+        # City: Not mapped
+        # Pages: Not mapped
+        # ThesisType: Mapped (type)
+        # StandardNumber: Mapped (number)
+        # Medium: Not mapped
+        # DOI: Not mapped
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "Department", self.committee)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Institution", self.organization)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Publisher", self.organization)
+        add_bibliography_namespaced_element_if_missing(_source_element, "ThesisType", self.type)
+        add_bibliography_namespaced_element_if_missing(_source_element, "StandardNumber", self.number)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Department", self.committee)

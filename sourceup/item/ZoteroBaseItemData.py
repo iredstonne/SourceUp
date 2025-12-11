c1db3e85c430703c52649a251434d0da4fb11443
iredstonne <iredstonne.dev@gmail.com>
2025-12-11 22:57:24 +0100

    feat(item data > base): Add book author, artist, director, performer, inventor and rename language to LCID (word BibXML naming)

--- sourceup/item/ZoteroBaseItemData.py
+++ sourceup/item/ZoteroBaseItemData.py
@@ -62,6 +62,20 @@ class ZoteroBaseItemData:
         )
 
     def map_to_bibxml(self, _source_element: Element):
+        # SourceType: Mapped (delegated)
+        # Creators: Partially mapped (not delegated yet)
+        # Title: Mapped (title)
+        # Year: Mapped (date -> year)
+        # Month: Mapped (date -> month)
+        # Day: Mapped (date -> day)
+        # Language: Mapped
+        # ShortTitle: Mapped
+        # YearAccessed: Mapped (access_date -> year)
+        # MonthAccessed: Mapped (access_date -> month)
+        # DayAccessed: Mapped (access_date -> day)
+        # Comments: Mapped
+        # URL: Mapped
+
         _source_type_element = create_bibliography_namespaced_element("SourceType")
         _source_type_element.text = self.bibliography_source_type()
         _source_element.append(_source_type_element)
@@ -109,6 +123,31 @@ class ZoteroBaseItemData:
 
                 if list(_author_role_element):
                     _author_composite_element.append(_author_role_element)
+
+            # Role: Book Author
+            _book_author_creators = [
+                _creator for _creator in self.creators
+                if _creator.creator_type == ZoteroCreatorType.AUTHOR
+            ]
+
+            if _book_author_creators:
+                _person_book_author_creators = [
+                    _book_author_creator for _book_author_creator in _book_author_creators
+                    if isinstance(_book_author_creator.creator_data, ZoteroPersonCreatorData)
+                ]
+
+                _book_author_role_element = create_bibliography_namespaced_element("Bookauthor")
+
+                if _person_book_author_creators:
+                    _name_list_element = create_bibliography_namespaced_element("NameList")
+
+                    for _creator in _person_book_author_creators:
+                        _creator.creator_data.map_to_bibxml(_name_list_element)
+
+                    _book_author_role_element.append(_name_list_element)
+
+                if list(_book_author_role_element):
+                    _author_composite_element.append(_book_author_role_element)
 
             # Role: Editor
             _editor_creators = [
@@ -160,105 +199,159 @@ class ZoteroBaseItemData:
                 if list(_producer_role_element):
                     _author_composite_element.append(_producer_role_element)
 
+            # Role: Artist
+            _artist_creators = [
+                _creator for _creator in self.creators
+                if _creator.creator_type == ZoteroCreatorType.ARTIST
+            ]
+
+            if _artist_creators:
+                _person_artist_creators = [
+                    _artist_creator for _artist_creator in _artist_creators
+                    if isinstance(_artist_creator.creator_data, ZoteroPersonCreatorData)
+                ]
+
+                _artist_role_element = create_bibliography_namespaced_element("Artist")
+
+                if _person_artist_creators:
+                    _name_list_element = create_bibliography_namespaced_element("NameList")
+
+                    for _creator in _person_artist_creators:
+                        _creator.creator_data.map_to_bibxml(_name_list_element)
+
+                    _artist_role_element.append(_name_list_element)
+
+                if list(_artist_role_element):
+                    _author_composite_element.append(_artist_role_element)
+
+            # Role: Director
+            _director_creators = [
+                _creator for _creator in self.creators
+                if _creator.creator_type == ZoteroCreatorType.DIRECTOR
+            ]
+
+            if _director_creators:
+                _person_director_creators = [
+                    _performer_creator for _performer_creator in _director_creators
+                    if isinstance(_performer_creator.creator_data, ZoteroPersonCreatorData)
+                ]
+
+                _director_role_element = create_bibliography_namespaced_element("Director")
+
+                if _person_director_creators:
+                    _name_list_element = create_bibliography_namespaced_element("NameList")
+
+                    for _creator in _person_director_creators:
+                        _creator.creator_data.map_to_bibxml(_name_list_element)
+
+                    _director_role_element.append(_name_list_element)
+
+                if list(_director_role_element):
+                    _author_composite_element.append(_director_role_element)
+
+            # Role: Performer
+            _performer_creators = [
+                _creator for _creator in self.creators
+                if _creator.creator_type == ZoteroCreatorType.PERFORMER
+            ]
+
+            if _performer_creators:
+                _person_performer_creators = [
+                    _performer_creator for _performer_creator in _performer_creators
+                    if isinstance(_performer_creator.creator_data, ZoteroPersonCreatorData)
+                ]
+
+                _performer_role_element = create_bibliography_namespaced_element("Performer")
+
+                if _person_performer_creators:
+                    _name_list_element = create_bibliography_namespaced_element("NameList")
+
+                    for _creator in _person_performer_creators:
+                        _creator.creator_data.map_to_bibxml(_name_list_element)
+
+                    _performer_role_element.append(_name_list_element)
+
+                if list(_performer_role_element):
+                    _author_composite_element.append(_performer_role_element)
+
+            # Role: Inventor
+            _inventor_creators = [
+                _creator for _creator in self.creators
+                if _creator.creator_type == ZoteroCreatorType.INVENTOR
+            ]
+
+            if _inventor_creators:
+                _person_inventor_creators = [
+                    _inventor_creator for _inventor_creator in _inventor_creators
+                    if isinstance(_inventor_creator.creator_data, ZoteroPersonCreatorData)
+                ]
+
+                _inventor_role_element = create_bibliography_namespaced_element("Inventor")
+
+                if _person_inventor_creators:
+                    _name_list_element = create_bibliography_namespaced_element("NameList")
+
+                    for _creator in _person_inventor_creators:
+                        _creator.creator_data.map_to_bibxml(_name_list_element)
+
+                    _inventor_role_element.append(_name_list_element)
+
+                if list(_inventor_role_element):
+                    _author_composite_element.append(_inventor_role_element)
+
             if list(_author_composite_element):
                 _source_element.append(_author_composite_element)
-
-        if self.abstract_note:
-            # <b:Abstract>
-            _abstract_element = create_bibliography_namespaced_element("Abstract")
-            _abstract_element.text = str(self.abstract_note)
-            _source_element.append(_abstract_element)
 
         if self.date_to_datetime:
             _year = self.date_to_datetime.year
             if _year:
-                # <b:Year>
                 _year_element = create_bibliography_namespaced_element("Year")
                 _year_element.text = str(_year)
                 _source_element.append(_year_element)
             _month = self.date_to_datetime.month
             if _month:
-                # <b:Month>
                 _month_element = create_bibliography_namespaced_element("Month")
                 _month_element.text = str(_month)
                 _source_element.append(_month_element)
             _day = self.date_to_datetime.day
             if _day:
-                # <b:Day>
                 _day_element = create_bibliography_namespaced_element("Day")
                 _day_element.text = str(_day)
                 _source_element.append(_day_element)
 
         if self.language:
-            # <b:Language>
-            _language_element = create_bibliography_namespaced_element("Language")
-            _language_element.text = str(self.language)
-            _source_element.append(_language_element)
+            _lcid_element = create_bibliography_namespaced_element("LCID")
+            _lcid_element.text = str(self.language)
+            _source_element.append(_lcid_element)
 
         if self.short_title:
-            # <b:ShortTitle>
             _short_title_element = create_bibliography_namespaced_element("ShortTitle")
             _short_title_element.text = str(self.short_title)
             _source_element.append(_short_title_element)
 
-        if self.url:
-            # <b:URL>
-            _url_element = create_bibliography_namespaced_element("URL")
-            _url_element.text = str(self.url)
-            _source_element.append(_url_element)
-
         if self.access_date_to_datetime:
             _year_accessed = self.access_date_to_datetime.year
             if _year_accessed:
-                # <b:YearAccessed>
                 _year_accessed_element = create_bibliography_namespaced_element("YearAccessed")
                 _year_accessed_element.text = str(_year_accessed)
                 _source_element.append(_year_accessed_element)
             _month_accessed = self.access_date_to_datetime.month
             if _month_accessed:
-                # <b:MonthAccessed>
                 _month_accessed_element = create_bibliography_namespaced_element("MonthAccessed")
                 _month_accessed_element.text = str(_month_accessed)
                 _source_element.append(_month_accessed_element)
             _day_accessed = self.access_date_to_datetime.day
             if _day_accessed:
-                # <b:DayAccessed>
                 _day_accessed_element = create_bibliography_namespaced_element("DayAccessed")
                 _day_accessed_element.text = str(_day_accessed)
                 _source_element.append(_day_accessed_element)
 
-        if self.archive:
-            # <b:Archive>
-            _archive_element = create_bibliography_namespaced_element("Archive")
-            _archive_element.text = str(self.archive)
-            _source_element.append(_archive_element)
-
-        if self.archive_location:
-            # <b:ArchiveLocation>
-            _archive_location_element = create_bibliography_namespaced_element("ArchiveLocation")
-            _archive_location_element.text = str(self.archive_location)
-            _source_element.append(_archive_location_element)
-
-        if self.library_catalog:
-            # <b:LibraryCatalog>
-            _library_catalog_element = create_bibliography_namespaced_element("LibraryCatalog")
-            _library_catalog_element.text = str(self.library_catalog)
-            _source_element.append(_library_catalog_element)
-
-        if self.call_number:
-            # <b:CallNumber>
-            _call_number_element = create_bibliography_namespaced_element("CallNumber")
-            _call_number_element.text = str(self.call_number)
-            _source_element.append(_call_number_element)
-
-        if self.rights:
-            # <b:Rights>
-            _rights_element = create_bibliography_namespaced_element("Rights")
-            _rights_element.text = str(self.rights)
-            _source_element.append(_rights_element)
-
         if self.extra:
-            # <b:Comment>
-            _comment_element = create_bibliography_namespaced_element("Comment")
-            _comment_element.text = str(self.extra)
-            _source_element.append(_comment_element)
+            _comments_element = create_bibliography_namespaced_element("Comments")
+            _comments_element.text = str(self.extra)
+            _source_element.append(_comments_element)
+
+        if self.url:
+            _url_element = create_bibliography_namespaced_element("URL")
+            _url_element.text = str(self.url)
+            _source_element.append(_url_element)

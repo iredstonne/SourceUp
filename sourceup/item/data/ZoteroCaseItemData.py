262a4f044e8b737a22e9b8dc76000e94237f1fca
iredstonne <iredstonne.dev@gmail.com>
2025-12-11 22:47:21 +0100

    feat(item data > case): Full support of BibXML tags

--- sourceup/item/data/ZoteroCaseItemData.py
+++ sourceup/item/data/ZoteroCaseItemData.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.casts import map_to_str
@@ -22,6 +25,11 @@ class ZoteroCaseItemData(ZoteroBaseItemData):
 
     @override
     @classmethod
+    def bibliography_source_type(cls):
+        return "Misc"
+
+    @override
+    @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroCaseItemData":
         _base_item_data = ZoteroBaseItemData.map_from_data(_data)
         return cls(
@@ -36,3 +44,30 @@ class ZoteroCaseItemData(ZoteroBaseItemData):
             first_page=map_to_str(_data.get("firstPage")),
             history=map_to_str(_data.get("history"))
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        # SourceType -> Misc
+        # PublicationTitle: Mapped (reporter)
+        # MediaType: Mapped "Case"
+        # City: Not mapped
+        # StateProvince: Not mapped
+        # CountryRegion: Not mapped
+        # Publisher: Mapped (court)
+        # Pages: Mapped (first_page)
+        # Volume: Mapped (reporter_volume)
+        # Edition: Not mapped
+        # Number: Mapped (docket_number)
+        # StandardNumber: Mapped (docket_number)
+        # Medium: Not mapped
+        # DOI: Not mapped
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "PublicationTitle", self.reporter)
+        add_bibliography_namespaced_element_if_missing(_source_element, "MediaType", "Case")
+        add_bibliography_namespaced_element_if_missing(_source_element, "Publisher", self.court)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Pages", self.first_page)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Volume", self.reporter_volume)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Number", self.docket_number)
+        add_bibliography_namespaced_element_if_missing(_source_element, "StandardNumber", self.docket_number)

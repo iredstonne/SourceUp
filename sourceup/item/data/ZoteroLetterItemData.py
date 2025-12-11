83eafda8229a0379158c24024519bd530079f337
iredstonne <iredstonne.dev@gmail.com>
2025-12-11 22:49:36 +0100

    feat(item data > letter): Full support of BibXML tags

--- sourceup/item/data/ZoteroLetterItemData.py
+++ sourceup/item/data/ZoteroLetterItemData.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.casts import map_to_str
@@ -22,3 +25,25 @@ class ZoteroLetterItemData(ZoteroBaseItemData):
                for base_item_data_field in fields(ZoteroBaseItemData)},
             letter_type=map_to_str(_data.get("letterType")),
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        # SourceType -> Misc
+        # PublicationTitle: Mapped (title)
+        # MediaType: Mapped "Letter"
+        # City: Not mapped
+        # StateProvince: Not mapped
+        # CountryRegion: Not mapped
+        # Publisher: Not mapped
+        # Pages: Not mapped
+        # Volume: Not mapped
+        # Edition: Not mapped
+        # Number: Not mapped
+        # StandardNumber: Not mapped
+        # Medium: Mapped (letter_type)
+        # DOI: Not mapped
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "PublicationTitle", self.title)
+        add_bibliography_namespaced_element_if_missing(_source_element, "Medium", self.letter_type)

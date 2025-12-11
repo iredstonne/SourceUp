1216b80077e78b664fe502986c58fdca091743ea
iredstonne <iredstonne.dev@gmail.com>
2025-12-11 22:50:59 +0100

    feat(item data > radio broadcast): Full support of BibXML tags

--- sourceup/item/data/ZoteroRadioBroadcastItemData.py
+++ sourceup/item/data/ZoteroRadioBroadcastItemData.py
@@ -4,12 +4,16 @@ from xml.etree.ElementTree import Element
 
 from sourceup.casts import map_to_str
 from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
+from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.item.ZoteroItemType import ZoteroItemType
-from sourceup.item.data.ZoteroBroadcastItemData import ZoteroBroadcastItemData
-
 
 @dataclass(frozen=True, slots=True)
-class ZoteroRadioBroadcastItemData(ZoteroBroadcastItemData):
+class ZoteroRadioBroadcastItemData(ZoteroBaseItemData):
+    program_title: Optional[str] = None
+    episode_number: Optional[str] = None
+    place: Optional[str] = None
+    network: Optional[str] = None
+    running_time: Optional[str] = None
     audio_recording_format: Optional[str] = None
 
     @override
@@ -25,15 +29,36 @@ class ZoteroRadioBroadcastItemData(ZoteroBroadcastItemData):
     @override
     @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroRadioBroadcastItemData":
-        _broadcast_item_data = ZoteroBroadcastItemData.map_from_data(_data)
+        _base_item_data = ZoteroBaseItemData.map_from_data(_data)
         return cls(
-            **{_broadcast_item_data_field.name: getattr(_broadcast_item_data, _broadcast_item_data_field.name)
-               for _broadcast_item_data_field in fields(ZoteroBroadcastItemData)},
+            **{_base_item_data_field.name: getattr(_base_item_data, _base_item_data_field.name)
+               for _base_item_data_field in fields(ZoteroBaseItemData)},
+            program_title=map_to_str(_data.get("programTitle")),
+            episode_number=map_to_str(_data.get("episodeNumber")),
+            place=map_to_str(_data.get("place")),
+            network=map_to_str(_data.get("network")),
+            running_time=map_to_str(_data.get("runningTime")),
             audio_recording_format=map_to_str(_data.get("audioRecordingFormat"))
         )
 
     @override
     def map_to_bibxml(self, _source_element: Element):
-        ZoteroBroadcastItemData.map_to_bibxml(self, _source_element)
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
 
+        # SourceType -> SoundRecording
+        # AlbumTitle: Mapped (program_title)
+        # ProductionCompany: Mapped (network)
+        # Medium: Mapped (audio_recording_format)
+        # City: Mapped (place)
+        # StateProvince: Not mapped
+        # CountryRegion: Not mapped
+        # RecordingNumber: Mapped (call_number)
+        # StandardNumber: Not mapped
+        # DOI: Not mapped
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "AlbumTitle", self.program_title)
+        add_bibliography_namespaced_element_if_missing(_source_element, "ProductionCompany", self.network)
         add_bibliography_namespaced_element_if_missing(_source_element, "Medium", self.audio_recording_format)
+        add_bibliography_namespaced_element_if_missing(_source_element, "City", self.place)
+        add_bibliography_namespaced_element_if_missing(_source_element, "RecordingNumber", self.episode_number)
+

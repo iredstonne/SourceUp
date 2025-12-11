0f4c1db70a858b9776f40cf10d94a106cedce695
iredstonne <iredstonne.dev@gmail.com>
2025-12-11 22:51:20 +0100

    feat(item data > statue): Full support of BibXML tags

--- sourceup/item/data/ZoteroStatuteItemData.py
+++ sourceup/item/data/ZoteroStatuteItemData.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, fields
 from typing import Optional, override, Dict, Any
+from xml.etree.ElementTree import Element
+
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
 from sourceup.item.ZoteroItemType import ZoteroItemType
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.casts import map_to_str
@@ -22,13 +25,18 @@ class ZoteroStatuteItemData(ZoteroBaseItemData):
 
     @override
     @classmethod
+    def bibliography_source_type(cls):
+        return "Report"
+
+    @override
+    @classmethod
     def map_from_data(cls, _data: Dict[str, Any]) -> "ZoteroStatuteItemData":
         _base_item_data = ZoteroBaseItemData.map_from_data(_data)
         return cls(
             **{base_item_data_field.name: getattr(_base_item_data, base_item_data_field.name)
                for base_item_data_field in fields(ZoteroBaseItemData)},
             code=map_to_str(_data.get("code")),
-            code_number=map_to_str(_data.get("code_number")),
+            code_number=map_to_str(_data.get("codeNumber")),
             public_law_number=map_to_str(_data.get("publicLawNumber")),
             date_enacted=map_to_str(_data.get("dateEnacted")),
             pages=map_to_str(_data.get("pages")),
@@ -36,3 +44,21 @@ class ZoteroStatuteItemData(ZoteroBaseItemData):
             session=map_to_str(_data.get("session")),
             history=map_to_str(_data.get("history"))
         )
+
+    @override
+    def map_to_bibxml(self, _source_element: Element):
+        ZoteroBaseItemData.map_to_bibxml(self, _source_element)
+
+        # SourceType -> Report
+        # Department: Not mapped
+        # Institution: Not mapped
+        # Publisher: Not mapped
+        # City: Not mapped
+        # Pages: Mapped (pages)
+        # ThesisType: Not mapped
+        # StandardNumber: Mapped (public_law_number)
+        # Medium: Not mapped
+        # DOI: Not mapped
+
+        add_bibliography_namespaced_element_if_missing(_source_element, "Pages", self.pages)
+        add_bibliography_namespaced_element_if_missing(_source_element, "StandardNumber", self.public_law_number)

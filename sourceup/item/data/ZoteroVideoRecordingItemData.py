4f0250b967a23e426d169c57ec8a8a0c8906f985
iredstonne <iredstonne.dev@gmail.com>
2025-12-17 13:21:58 +0100

    feat(item data > video recording): Map creators used by item data

--- sourceup/item/data/ZoteroVideoRecordingItemData.py
+++ sourceup/item/data/ZoteroVideoRecordingItemData.py
@@ -3,7 +3,9 @@ from typing import Optional, override, Dict, Any
 from xml.etree.ElementTree import Element
 
 from sourceup.casts import map_to_str
-from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing
+from sourceup.creator.ZoteroCreatorType import ZoteroCreatorType
+from sourceup.exporter.wordbibxml_functions import add_bibliography_namespaced_element_if_missing, \
+    add_bibliography_namespaced_role_element
 from sourceup.item.ZoteroBaseItemData import ZoteroBaseItemData
 from sourceup.item.ZoteroItemType import ZoteroItemType
 
@@ -44,6 +46,22 @@ class ZoteroVideoRecordingItemData(ZoteroBaseItemData):
             studio=map_to_str(_data.get("studio")),
             video_recording_format=map_to_str(_data.get("videoRecordingFormat"))
         )
+
+    @override
+    def map_creators_to_bibxml(self, _author_composite_element: Element):
+        add_bibliography_namespaced_role_element(_author_composite_element, self.creators, (
+            ZoteroCreatorType.CAST_MEMBER,
+            ZoteroCreatorType.CONTRIBUTOR
+        ), "Performer", True)
+        add_bibliography_namespaced_role_element(_author_composite_element, self.creators, (
+            ZoteroCreatorType.PRODUCER,
+        ), "ProducerName", False)
+        add_bibliography_namespaced_role_element(_author_composite_element, self.creators, (
+            ZoteroCreatorType.DIRECTOR,
+        ), "Director", False)
+        add_bibliography_namespaced_role_element(_author_composite_element, self.creators, (
+            ZoteroCreatorType.SCRIPT_WRITER,
+        ), "Writer", False)
 
     @override
     def map_to_bibxml(self, _source_element: Element):

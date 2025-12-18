6f48cd4a9b56bf1f552a7cf30a2ae7816fc94b53
iredstonne <iredstonne.dev@gmail.com>
2025-12-18 14:41:47 +0100

    fix(app): Inline icon path

--- sourceup/app.py
+++ sourceup/app.py
@@ -13,15 +13,13 @@ from sourceup.ui.window.MainWindow import MainWindow
 def assets_path() -> Path:
     return Path(__file__).resolve().parent / "assets"
 
-ICON_PATH = assets_path() / "icon.png"
-
 def main():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     signal.signal(signal.SIGTERM, signal.SIG_DFL)
     app = QApplication()
     app.setApplicationName(APP_NAME)
     app.setApplicationVersion(APP_VERSION_TAG_NAME)
-    app.setWindowIcon(QIcon(str(ICON_PATH)))
+    app.setWindowIcon(QIcon(str(assets_path() / "icon.png")))
     check_if_new_update_is_available(APP_VERSION_TAG_NAME)
     main_window = MainWindow(app)
     main_window.show()

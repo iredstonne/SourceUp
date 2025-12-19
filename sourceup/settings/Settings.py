cab7fc57ad1477336d0a4c0dafa05ec4ac3b8991
iredstonne <iredstonne.dev@gmail.com>
2025-12-19 10:58:16 +0100

    feat(settings): Fix commit temp settings file to settings file by replacing it atomically

--- sourceup/settings/Settings.py
+++ sourceup/settings/Settings.py
@@ -1,48 +1,63 @@
+import atexit
 import os
 from pathlib import Path
+from typing import Optional
+
+from pydantic import ValidationError
+
+from sourceup.platform import _NT_KERNEL_try_set_hidden_flag
 from sourceup.settings.data.SettingsData import SettingsData
 
 class Settings:
-    def __init__(self, path: Path):
-        self.path = path
-        self.tmp_path = path.with_suffix(path.suffix + ".tmp")
+    def __init__(self, _path: Path):
+        self._tmp_path = _path.with_suffix(_path.suffix + ".tmp")
+        self._tmp_path.parent.mkdir(parents=True, exist_ok=True)
+        self._path = _path
+        self._path.parent.mkdir(parents=True, exist_ok=True)
+        atexit.register(self._dispose)
+
+    @staticmethod
+    def try_load(_path: Path) -> Optional[SettingsData]:
+        try:
+            if not _path.is_file():
+                return None
+            return SettingsData.model_validate_json(_path.read_bytes())
+        except (OSError, IOError, ValueError, ValidationError):
+            return None
 
     def load(self) -> SettingsData:
-        if not self.path.is_file():
-            return SettingsData()
-        return SettingsData.model_validate_json(self.path.read_bytes())
+        _tmp_data = self.try_load(self._tmp_path)
+        if _tmp_data is not None:
+            self._commit()
+            return _tmp_data
+        _data = self.try_load(self._path)
+        return _data or SettingsData()
 
-    def save(self, settings_data: SettingsData):
-        self.path.parent.mkdir(parents=True, exist_ok=True)
-        data = settings_data.model_dump_json(indent=2).encode("utf-8")
-        with open(self.tmp_path, "wb") as file:
-            file.write(data)
-            file.flush()
-            os.fsync(file.fileno())
-        self._try_hide_tmp_file_on_nt()
-        os.replace(self.tmp_path, self.path)
-
-
-    def dispose(self):
+    def save(self, _settings_data: SettingsData):
         try:
-            if self.tmp_path.exists():
-                self.tmp_path.unlink()
-        except OSError:
+            self._tmp_path.parent.mkdir(parents=True, exist_ok=True)
+            with open(self._tmp_path, "wb") as _file:
+                _file.write(_settings_data.model_dump_json(indent=2).encode("utf-8"))
+                _file.flush()
+                os.fsync(_file.fileno())
+            _NT_KERNEL_try_set_hidden_flag(self._tmp_path, True)
+        except (OSError, IOError):
             pass
 
-    def _try_hide_tmp_file_on_nt(self):
-        if os.name != "nt":
-            return
+    def _commit(self) -> bool:
         try:
-            import ctypes
-            from ctypes import wintypes
-            set_file_attributes = ctypes.windll.kernel32.SetFileAttributesW # type: ignore[attr-defined]
-            # Windows NT kernel terminology
-            # LPCWSTR = Long Pointer Constant Wide (UTF-16) String (= Unicode string)
-            # DWORD = Double 16-bit value (= 32 bit unsigned int)
-            set_file_attributes.argtypes = (wintypes.LPCWSTR, wintypes.DWORD)
-            set_file_attributes.restype = wintypes.BOOL
-            FILE_ATTRIBUTE_HIDDEN = 0x02
-            set_file_attributes(str(self.tmp_path), FILE_ATTRIBUTE_HIDDEN)
-        except (OSError, ImportError, AttributeError, TypeError):
+            if self._tmp_path.is_file():
+                self._tmp_path.parent.mkdir(parents=True, exist_ok=True)
+                self._path.parent.mkdir(parents=True, exist_ok=True)
+                os.replace(self._tmp_path, self._path)
+                _NT_KERNEL_try_set_hidden_flag(self._path, False)
+            return True
+        except (OSError, IOError):
+            return False
+
+    def _dispose(self):
+        try:
+            if self._tmp_path.is_file() and self._commit():
+                self._tmp_path.unlink()
+        except (OSError, IOError):
             pass
